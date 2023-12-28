5299b8d22c49ffc1577576ed4031efd76af85d4a
Neel Bhanushali <neel@atriina.com>
2023-12-28 17:01:19 +0530

    Created User controller and service manually Used controller and service commands for product Used resource command without endpoints for order Used resource command with endpoints for invoices

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { ProductController } from './product/product.controller';
+import { ProductService } from './product/product.service';
+import { OrderModule } from './order/order.module';
+import { InvoiceModule } from './invoice/invoice.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  imports: [OrderModule, InvoiceModule],
+  controllers: [AppController, UserController, ProductController],
+  providers: [AppService, UserService, ProductService],
 })
 export class AppModule {}
